36178bd4cecb717c97dfa3bc3f07a215fdb660ce
Kunal <kunalpatil218@gmail.com>
2020-10-24 23:41:59 +0100

    Added Gremlin Java Connector Code

---
+++ Neo4J/GremlinJava/src/main/java/com/gremlin/api/GremlinJavaConnector.java
@@ -0,0 +1,45 @@
+package com.gremlin.api;
+
+import org.apache.tinkerpop.gremlin.driver.Client;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.Result;
+import org.apache.tinkerpop.gremlin.driver.ResultSet;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class GremlinJavaConnector {
+
+    public  boolean connect(String query){
+        Cluster cluster = null;
+        try {
+            cluster = Cluster.open("C:\\Users\\GS-2022\\Pictures\\Neo4j Data Learning\\GremlinJava\\src\\main\\resources\\remote.yaml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Client client = cluster.connect();
+        ResultSet result = client.submit("1");
+        CompletableFuture<List<Result>> output = result.all();
+
+        try {
+            for (Result s : output.get()) {
+                System.out.println(s.toString());
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return  true;
+    }
+    public void run(){
+
+    }
+    public static void main(String[] args) {
+
+        String query = "g.V().hasLabel(\\\"Category\\\").values(\\\"categoryName\\\")";
+    GremlinJavaConnector gremlinJavaConnector = new GremlinJavaConnector();
+    gremlinJavaConnector.connect(query);
+    }
+}
